71a589743dfd06b4a7bff80e53ca966bb3e7b4c7
Jiri (George) Lebl <jiri.lebl@gmail.com>
2025-04-17 15:52:24 -0500

    Add some bindings

--- config.h
+++ config.h
@@ -164,7 +164,9 @@ static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button1,        spawn,          {.v = my_menu } },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkWinTitle,          0,              Button3,        spawn,          {.v = my_rofi } },
 	{ ClkStatusText,        0,              Button1,        spawn,          {.v = my_calcmd } },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkStatusText,        0,              Button3,        spawn,          {.v = my_wttrcmd } },
