2a634fd5f66196b15c60f6031b3e79212dfb27fa
Jiri (George) Lebl <jiri.lebl@gmail.com>
2025-08-24 19:08:11 -0500

    whack the killclient with Super-Shift-c as I never use that

--- config.h
+++ config.h
@@ -119,7 +119,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,	   view,           {0} },
 	{ Mod1Mask,                     XK_grave,  spawn,          {.v = my_rofitab } },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	/*{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },*/
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
